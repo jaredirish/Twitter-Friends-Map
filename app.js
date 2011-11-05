68d80334dfbe57cd6e38b09e0e6381b24271f6f5
Aaron Ackerman <theron17@gmail.com>
2011-11-05 11:49:48 -0500

    first page commit

--- app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.sn-button').bind('click', function(){
-       twitterFriends = new TwitterFriends($('.sn-input').val());
-       $('.sn-button').fadeOut();
-       $('.sn-input').fadeOut();
-    });
-});
