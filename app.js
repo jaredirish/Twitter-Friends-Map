e62c450543d47858243951bf80b9eaec410ac34f
Aaron Ackerman <theron17@gmail.com>
2011-11-07 22:42:26 -0600

    added working auth for Twitter @anywhere

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ $(document).ready(function(){
           $(".twitter-login").append(profileImageTag + screenNameTemp);
           twitterFriends = new TwitterFriends(screenName);
         } else {
-          T(".twitter-login").connectButton();
+          T(".twitter-login").connectButton({
+              authComplete: function(user) {
+                  currentUser = user;
+                  screenName = currentUser.data('screen_name');
+                  profileImage = currentUser.data('profile_image_url');
+                  profileImageTag = "<img src='" + profileImage + "'/>";
+                  screenNameTemp = '<a class="logged-sn">'+screenName+'</a><div class="followers"></div>';
+                  $(".twitter-login").append(profileImageTag + screenNameTemp);
+                  $('.twitter-login iframe').remove();
+                  twitterFriends = new TwitterFriends(screenName);
+              } 
+          });
         };
     });
     $('.sn-button').bind('click', function(){
